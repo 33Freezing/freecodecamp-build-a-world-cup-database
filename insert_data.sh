5d696bbf5b8c53257d7a04b06558c6cf74d7ebf1
33Freezing <63356475+33Freezing@users.noreply.github.com>
2025-10-29 12:22:57 +0100

    Add script to insert game data into database
    Insert data from games.csv into the database, handling team IDs.

---
+++ insert_data.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+$PSQL "truncate teams, games;"
+
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != "year" ]] 
+    then 
+    TEAM_WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER';")
+    if [[ -z $TEAM_WINNER_ID ]]
+      then
+      $PSQL "insert into teams(name) values('$WINNER');"
+      TEAM_WINNER_ID=$($PSQL "select team_id from teams where name='$WINNER'")
+    fi
+    TEAM_OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT';")
+    if [[ -z $TEAM_OPPONENT_ID ]]
+      then
+      $PSQL "insert into teams(name) values('$OPPONENT');"
+      TEAM_OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+    fi
+
+  $PSQL "insert into games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values($YEAR, '$ROUND', $TEAM_WINNER_ID, $TEAM_OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);"
+
+
+
+  fi
+done
